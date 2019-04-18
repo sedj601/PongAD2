6adf23ca04111c8379c3458e25fc6cf96d852f60
Sedrick Jefferson <blj0011@auburn.edu>
2019-04-18 11:21:51 -0500

    first commit

--- app/src/main/java/sed/krappy/games/pong/MainActivity.java
+++ app/src/main/java/sed/krappy/games/pong/MainActivity.java
@@ -7,50 +7,19 @@ import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.KeyEvent;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
-
-import java.security.Key;
 import java.text.DecimalFormat;
 import java.util.HashSet;
 import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
-
-    // desired fps
-    private final static int    MAX_FPS = 50;
-    // maximum number of frames to be skipped
-    private final static int    MAX_FRAME_SKIPS = 5;
-    // the frame period
-    private final static int    FRAME_PERIOD = 1000 / MAX_FPS;
-    // Stuff for stats */
-    private DecimalFormat df = new DecimalFormat("0.##");  // 2 dp
-    // we'll be reading the stats every second
-    private final static int    STAT_INTERVAL = 1000; //ms
-    // the average will be calculated by storing
-    // the last n FPSs
-    private final static int    FPS_HISTORY_NR = 10;
-    // last time the status was stored
-    private long lastStatusStore = 0;
-    // the status time counter
-    private long statusIntervalTimer    = 0l;
-    // number of frames skipped since the game started
-    private long totalFramesSkipped         = 0l;
-    // number of frames skipped in a store cycle (1 sec)
-    private long framesSkippedPerStatCycle  = 0l;
-
-    // number of rendered frames in an interval
-    private int frameCountPerStatCycle = 0;
-    private long totalFrameCount = 0l;
-    // the last FPS values
-    private double  fpsStore[];
-    // the number of times the stat has been read
-    private long    statsCount = 0;
-    // the average FPS since the game started
-    private double  averageFps = 0.0;
+    double interpolation = 0;
+    int TICKS_PER_SECONDS = 50;
+    int SKIP_TICKS = 1000 / TICKS_PER_SECONDS;
+    int MAX_FRAME_SKIPS = 10;
 
     int ballXDirection = 1;
     int ballYDirection = 1;
@@ -61,7 +30,6 @@ public class MainActivity extends AppCompatActivity {
 
     PongAIPlayer leftPlayer, rightPlayer;
 
-    AsyncTask asyncTask;
     Runnable runnable;
     boolean running;
 
@@ -97,53 +65,23 @@ public class MainActivity extends AppCompatActivity {
         runnable = new Runnable() {
             @Override
             public void run() {
-                long beginTime;
-                long timeDiff;
-                int sleepTime = 0;
-                int framesSkipped;
-                synchronized (this) {
-                    try {
-                        while (running) {
-                            beginTime = System.currentTimeMillis();
-                            framesSkipped = 0;
+                double next_game_tick = System.currentTimeMillis();
+                int loops;
 
-                            moveBall();
-                            //movePaddles();
-                            aiPaddlesMove();
-
-                            timeDiff = System.currentTimeMillis() - beginTime;
-                            sleepTime = (int) ((1000 / 50) - timeDiff);
-                            if (sleepTime > 0) {
-                                try {
-                                    Thread.sleep(sleepTime);
-                                } catch (InterruptedException ex) {
-                                    //Log.d("MainActivity", ex.toString());
-                                    //Log.d("",Integer.toString(framesSkipped));
-                                    //Log.d("MainActivity", "Thread shutting down as it was requested to stop.");
-                                }
-//                        finally {
-//                            gameThread = null;
-//                        }
-                            }
-                            if (!running) {
-                                this.wait();
-                            }
-
-                            while (sleepTime < 0 && framesSkipped < MAX_FRAME_SKIPS) {
-                                sleepTime += FRAME_PERIOD;
-                                framesSkipped++;
-                            }
-
-                            if (framesSkipped > 0) {
-                                Log.d("MainActivity", Integer.toString(framesSkipped));
-                            }
-
-                            framesSkippedPerStatCycle += framesSkipped;
-                        }
-                    }catch (InterruptedException ex)
+                while (true)
+                {
+                    loops = 0;
+                    while(System.currentTimeMillis() > next_game_tick && loops < MAX_FRAME_SKIPS)
                     {
-                        ex.printStackTrace();
+                        moveBall();
+                        //movePaddles();
+                        aiPaddlesMove();
+
+                        next_game_tick += SKIP_TICKS;
+                        loops++;
                     }
+
+                    interpolation = System.currentTimeMillis() + SKIP_TICKS - next_game_tick  / (double)SKIP_TICKS;
                 }
             }
         };
@@ -201,7 +139,16 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    private void aiPaddlesMove()
+    public void moveBall()
+    {
+        ivBall.setX((float) (ivBall.getX() + (ballXDirection * ballXSpeed * Math.cos(bounceAngle))));
+        ivBall.setY((float)( ivBall.getY() + (ballYDirection * ballYSpeed * -Math.sin(bounceAngle))));
+        //Log.d("MainActivity",ivBall.getX() + " - " + ivBall.getY() + " - " + (clField.getY() + clField.getHeight()));
+        checkPaddleCollision();
+        checkWallCollision();
+    }
+
+    public void aiPaddlesMove()
     {
         if (ballXDirection < 0) {
             leftPlayer.MovePaddle(true, ivBall.getX(), ivBall.getY() + (ivBall.getHeight() / 2));
@@ -221,11 +168,9 @@ public class MainActivity extends AppCompatActivity {
         else if(inputs.contains("DOWN_ARROW_PRESSED")){
             ivPaddleLeft.setY(ivPaddleLeft.getY() + 10);
         }
-
-
     }
 
-    public void checkPaddleCollision()
+    private void checkPaddleCollision()
     {
         //Log.d("MainActivity",clField.getWidth() + "");
         Rect rectBall = new Rect();
@@ -237,6 +182,8 @@ public class MainActivity extends AppCompatActivity {
 
         if(Rect.intersects(rectBall, rectPaddleRight))
         {
+            Log.d("MainActivity", "paddle right: " + ivBall.getX() + " :: " + ivPaddleRight.getX());
+            //ivBall.setX(ivBall.getX() + ivBall.getWidth() - (ivBall.getX() + ivBall.getWidth() - ivPaddleRight.getX()));
             ballXDirection *= -1;
             double paddleIntersectY = (ivBall.getY() + (ivBall.getHeight() / 2)) - ivPaddleRight.getY();
             double relativeIntersectY = paddleIntersectY - (ivPaddleRight.getHeight() / 2);
@@ -246,6 +193,8 @@ public class MainActivity extends AppCompatActivity {
         }
         else if(Rect.intersects(rectBall, rectPaddleLeft))
         {
+            //Log.d("MainActivity", "paddle left: " + ivBall.getX() + " :: " + (ivPaddleLeft.getX() + ivPaddleLeft.getWidth()));
+            ivBall.setX(ivBall.getX() + (ivPaddleLeft.getX() + ivPaddleLeft.getWidth() - ivBall.getX()));
             ballXDirection *= -1;
             double paddleIntersectY = (ivBall.getY() + (ivBall.getHeight() / 2)) - ivPaddleLeft.getY();
             double relativeIntersectY = paddleIntersectY - (ivPaddleLeft.getHeight() / 2);
@@ -254,7 +203,7 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    public void checkWallCollision()
+    private void checkWallCollision()
     {
         if(ivBall.getY() <= clField.getY()) {
             ballYDirection *= -1;
@@ -272,14 +221,4 @@ public class MainActivity extends AppCompatActivity {
             gameThread.interrupt();
         }
     }
-
-    public void moveBall()
-    {
-        ivBall.setX((float) (ivBall.getX() + (ballXDirection * ballXSpeed * Math.cos(bounceAngle))));
-        ivBall.setY((float)( ivBall.getY() + (ballYDirection * ballYSpeed * -Math.sin(bounceAngle))));
-        Log.d("MainActivity",ivBall.getX() + " - " + ivBall.getY() + " - " + (clField.getY() + clField.getHeight()));
-        checkPaddleCollision();
-        checkWallCollision();
-    }
-
 }
--- app/src/main/java/sed/krappy/games/pong/PongAIPlayer.java
+++ app/src/main/java/sed/krappy/games/pong/PongAIPlayer.java
@@ -13,10 +13,12 @@ public class PongAIPlayer
     private final ImageView paddle;
     private boolean isHitLocationCalculated = false;
     private double paddleHitLocation;
+    private int ySpeed;
 
     public PongAIPlayer(ImageView paddle)
     {
         this.paddle = paddle;
+        ySpeed = 12;
     }
 
     public void MovePaddle(boolean isBallComingAtPaddle, double ballLocationX, double ballLocationY)
@@ -25,16 +27,15 @@ public class PongAIPlayer
         if (isBallComingAtPaddle) {
 
             if (!isHitLocationCalculated) {
-                paddleHitLocation = this.paddle.getHeight() * ThreadLocalRandom.current().nextDouble(.40,.61);
+                paddleHitLocation = this.paddle.getHeight() * ThreadLocalRandom.current().nextDouble(.25,.75);
                 isHitLocationCalculated = true;
-                System.out.println(paddleHitLocation);
             }
 
             if (paddle.getY() + paddleHitLocation > ballLocationY) {
-                this.paddle.setY(this.paddle.getY() - 10);
+                this.paddle.setY(this.paddle.getY() - ySpeed);
             }
             else if (paddle.getY() + paddle.getHeight() - paddleHitLocation < ballLocationY) {
-                this.paddle.setY(this.paddle.getY() + 10);
+                this.paddle.setY(this.paddle.getY() + ySpeed);
             }
         }
         else {
